bb091c255b83494ef617ad9321e421358b71136b
PashaZorin <zorin5226@gmail.com>
2022-07-09 14:34:16 +0300

    not correct rending to AboutDesign

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AboutDesign from "./AboutDesign";
 import AboutPage from "./AboutPage";
 
 const Main = () => {
   return (
     <main>
       <AboutPage />
+      <AboutDesign />
     </main>
   );
 };
