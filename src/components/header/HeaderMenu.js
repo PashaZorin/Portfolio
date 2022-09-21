c803b2aa6fe68fac08dd7a9fc2de76650ada0496
PashaZorin <zorin5226@gmail.com>
2022-09-21 03:18:26 +0300

    tried adaptive header menu

--- src/components/header/HeaderMenu.js
+++ src/components/header/HeaderMenu.js
@@ -4,13 +4,14 @@ import Button from "../Button";
 const HeaderMenu = () => {
   return (
     <menu className="header__menu-conteiner">
-      <ul className="header__menu">
+      <ul className="header__menu ">
         <li className="header__menu-item">Услуги</li>
         <li>Портфолио</li>
         <li>Этапы</li>
         <li>Дизайнеры</li>
       </ul>
       <Button text="Заказать" className="header__menu-btn" />
+      <span />
     </menu>
   );
 };
