1ff5e7480c9169fbd6e8356420d2d117a4ccb415
PashaZorin <zorin5226@gmail.com>
2022-09-22 18:17:54 +0300

    header adaptive / aboutPage not adaptive

--- src/components/header/HeaderMenu.js
+++ src/components/header/HeaderMenu.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 
 const HeaderMenu = () => {
+  const [menuMobActive, setMenuMobActive] = useState(false);
+
   return (
     <menu className="header__menu-conteiner">
-      <ul className="header__menu ">
+      <ul
+        className={
+          !menuMobActive
+            ? "header__menu"
+            : ["header__menu", "header__menu-active"].join(" ")
+        }
+      >
         <li className="header__menu-item">Услуги</li>
         <li>Портфолио</li>
         <li>Этапы</li>
         <li>Дизайнеры</li>
       </ul>
       <Button text="Заказать" className="header__menu-btn" />
-      <span />
+      <div
+        className={
+          !menuMobActive
+            ? "header__dots"
+            : ["header__dots", "header__dots-active"].join(" ")
+        }
+        onClick={() => setMenuMobActive((prev) => !prev)}
+      >
+        <span />
+      </div>
     </menu>
   );
 };
