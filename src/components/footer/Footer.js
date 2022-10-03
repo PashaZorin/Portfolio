0363ed7912e7f5c573ff8c240b98e7b059daa836
PashaZorin <zorin5226@gmail.com>
2022-10-03 21:27:31 +0300

    adaptive Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,35 +10,38 @@ import instagram from "../../icons/instagram.svg";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__conteiner conteiner">
-        <div className="footer__information-main">
-          <Logo className="footer__logo" />
-          <div className="footer__contacts">
-            <div className="footer__contacts-item">
-              <img src={viber} alt="viber" />
-              <p>Валерия : +380505859409</p>
+      <div className="conteiner">
+        <div className="footer__conteiner">
+          <div className="footer__information-main">
+            <Logo className="footer__logo" />
+            <div className="footer__contacts">
+              <div className="footer__contacts-item">
+                <img src={viber} alt="viber" />
+                <p>Валерия : +380505859409</p>
+              </div>
+              <div className="footer__contacts-item">
+                <img src={viber} alt="viber" />
+                <p>Анна: +380505859409</p>
+              </div>
+              <div className="footer__contacts-item">
+                <img src={viber} alt="viber" />
+                <p>Валерия: +380505859409</p>
+              </div>
             </div>
-            <div className="footer__contacts-item">
+            <div className="footer__messengers">
+              <img src={instagram} alt="instagram" />
+              <img src={facebook} alt="facebook" />
+              <img src={telegram} alt="telegram" />
               <img src={viber} alt="viber" />
-              <p>Анна: +380505859409</p>
-            </div>
-            <div className="footer__contacts-item">
-              <img src={viber} alt="viber" />
-              <p>Валерия: +380505859409</p>
+              <img src={whatsapp} alt="whatsapp" />
             </div>
           </div>
-          <div className="footer__messengers">
-            <img src={instagram} alt="instagram" />
-            <img src={facebook} alt="facebook" />
-            <img src={telegram} alt="telegram" />
-            <img src={viber} alt="viber" />
-            <img src={whatsapp} alt="whatsapp" />
+          <div className="footer__information-additional">
+            <p>hr@dv-s.com</p>
           </div>
-        </div>
-        <div className="footer__information-additional">
-          <p>hr@dv-s.com</p>
         </div>
       </div>
+
       <p className="footer__copyright ">
         Developers 2018-2022 &copy; All rights reserved
       </p>
