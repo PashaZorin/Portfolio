3fe7eca48275054efa0ba45738687d37ce48d44a
PashaZorin <zorin5226@gmail.com>
2022-10-05 17:18:49 +0300

    tryid fixed animation in AboutDisign

--- src/components/main/DesignItem.js
+++ src/components/main/DesignItem.js
@@ -3,18 +3,22 @@ import Arrow from "../vectors/Arrow";
 
 const DesignItem = ({ id, title, text, isOpen, fun }) => {
   return (
-    <div className={`design__item ${isOpen ? "open" : ""}`}>
-      <div className="design__item-conteiner" onClick={() => fun(id)}>
-        <h4 className={`design__item-title ${isOpen ? "open" : ""}`}>
-          {title}
-        </h4>
-        <div>
-          <Arrow className={`design__item-errow ${isOpen ? "open" : ""}`} />
+    <div className="design__content-conteiner">
+      <div className={`design__item ${isOpen ? "open" : ""}`}>
+        <div className="design__item-conteiner" onClick={() => fun(id)}>
+          <h4 className={`design__item-title ${isOpen ? "open" : ""}`}>
+            {title}
+          </h4>
+          <div>
+            <Arrow className={`design__item-errow ${isOpen ? "open" : ""}`} />
+          </div>
         </div>
       </div>
-      <p className={`design__item-transcription ${isOpen ? "open" : ""}`}>
-        {text}
-      </p>
+      <div
+        className={`design__transcription-conteiner ${isOpen ? "open" : ""}`}
+      >
+        <p className="design__transcription">{text}</p>
+      </div>
     </div>
   );
 };
