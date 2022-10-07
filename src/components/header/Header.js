eff17953fb42ddcb6b9571b4d22cbe07989ea5a7
PashaZorin <zorin5226@gmail.com>
2022-10-07 15:21:23 +0300

    added header fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Logo from "../vectors/Logo";
 import HeaderMenu from "./HeaderMenu";
 import "../../styles/header.scss";
 
 const Header = () => {
+  const [headerFixed, setHeaderFixed] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    if (window.pageYOffset > 95) {
+      if (scrollPosition < window.pageYOffset) {
+        setScrollPosition(window.pageYOffset);
+        setHeaderFixed(false);
+      }
+      if (scrollPosition > window.pageYOffset) {
+        setHeaderFixed(true);
+        setScrollPosition(window.pageYOffset);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [window.pageYOffset]);
   return (
-    <header className="header">
+    <header className={headerFixed ? "header show" : "header "}>
       <div className="header__conteiner conteiner">
         <Logo className="header__logo" />
         <HeaderMenu />
